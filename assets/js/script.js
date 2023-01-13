0940905b8696a06f3d1e1eeaf191ea601b69da32
mark279455 <mark279455@ntlworld.com>
2023-01-13 19:04:54 +0000

    fixed bug with previous move square colours and squares clicked by mistake

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 let chessboard = document.getElementById('chessboard');
 let possibleToMoveTo = [];
 let movesUsed = 0;
+let moveHistory = [];
 let currentSquare;
 let height = 8;
 let width = 8;
-const globalDebugLevel = 1;
+const globalDebugLevel = 3;
 // let lightColor = "#00ffff";
 // let darkColor =  "#ee31db";
 // let hintColor = "#fefa67";
 let lightColor = "#ffffff";
-let darkColor = "#00ffff";
-let hintColor = "#c0c0c0";
+let darkColor = "#C0C0C0";
+let hintColor = "#00ff00";
+let usedColor = "#ff0000";
+
 createBoard();
 
 /**
@@ -49,21 +52,20 @@ function createBoard() {
             setSquareColor(chessSquare);
 
             // shows square ids in square - for debugging only
-            chessSquare.style.fontSize = "0.6rem";
-            chessSquare.textContent = chessSquare.id;
+            // chessSquare.style.fontSize = "0.8rem";
+            // chessSquare.textContent = chessSquare.id;
             // add to board
             chessboard.appendChild(chessSquare);
         }
     }
     // add event listeners to all squares
     document.addEventListener("click", function listenAllSquares(event) {
-        // sets the square clicked to the global var currentSquare for this turn
-        currentSquare = document.getElementById(event.target.id);
+        // adds the square clicked to the global array moveHistory
         if (possibleToMoveTo.includes(event.target.id) || movesUsed === 0) {
+            moveHistory.push(document.getElementById(event.target.id));
             setCurrentPosition();
         }
     });
-
     debugMessage(1, "\tcreateBoard() end");
 }
 
@@ -154,17 +156,27 @@ function rgb2hex(rgb) {
  */
 function setCurrentPosition() {
     debugMessage(2, "\n\tsetCurrentPosition start");
+    // set previous square's knight to black
+    if (moveHistory.length >1) {
+        let lastSq = moveHistory[moveHistory.length-2];
+        lastSq.getElementsByClassName("fa-chess-knight")[0].style.color = usedColor;
+        debugMessage(3, "removing square ref ["+lastSq.id+"] from moveHistory");
+    }
+    currentSquare = moveHistory[moveHistory.length-1];
+    debugMessage(3, "currentSquare = ["+currentSquare.id+"]");
+
     // increment the number of moves
     movesUsed++;
     // put knight in square
     currentSquare.innerHTML = "<i class='fa-solid fa-chess-knight'></i>";
+    currentSquare.getElementsByClassName("fa-chess-knight")[0].style.color = "blue";
     debugMessage(3, "currentSquare.style.height = " + currentSquare.style.height);
     // change size of knight according to square size
     changeFontAwesomeFontSize();
     // set the square background to remove the hint color
     setSquareColor(currentSquare);
     debugMessage(3, "currentSquare " + typeof currentSquare);
-    debugMessage(3, "currentPos move[" + movesUsed + "] = " + currentSquare.id);
+    debugMessage(3, "currentSquare move[" + movesUsed + "] = " + currentSquare.id);
     // set this square as visited
     setVisited();
     // add formatting to square
@@ -181,7 +193,7 @@ function setCurrentPosition() {
  * sets the font size to the square size - 10
  */
 function changeFontAwesomeFontSize() {
-    debugMessage(2, "\n\tchangeFontAwesomeFontSize start");
+    debugMessage(2, "\tchangeFontAwesomeFontSize start");
     const numberPattern = /\d+/g;
     debugMessage(3, "currentSquare.height = " + currentSquare.style.height);
     let squareSize = currentSquare.style.height.match(numberPattern);
@@ -189,7 +201,7 @@ function changeFontAwesomeFontSize() {
     let fontSize = (squareSize - 10) + "px";
     debugMessage(3, "fontSize = " + fontSize);
     currentSquare.style.fontSize = (squareSize - 10) + "px";
-    debugMessage(2, "\n\tchangeFontAwesomeFontSize end");
+    debugMessage(2, "\tchangeFontAwesomeFontSize end");
 }
 
 /**
@@ -230,6 +242,7 @@ function getPossibleToMoveTo() {
     }
     debugMessage(2, "\tgetPossibleToMoveTo end");
 }
+
 /**
  * are co-ordinates v - inside the limits of the board height and width
  * @param {*} v 
@@ -243,7 +256,6 @@ function isSquareInsideBoard(v, h) {
     }
     return false;
 }
-
 
 /**
  * have this square been visited - accents square element or string id
@@ -276,16 +288,14 @@ function clearOldPossibleMoves() {
     debugMessage(2, "\tclearOldPossibleMoves end");
 }
 
-
-
 /**
  * game over
  */
 function gameOver() {
     debugMessage(2, "movesUsed = " + movesUsed);
-    if (movesUsed == 64) {
-        document.getElementById("gameover").textContent = "You completed the Knight's Tour";
+    if (movesUsed == height * width) {
+        document.getElementById("gameover").textContent = "You completed a Knight's Tour of " + height + " by " + width;
     } else {
-        document.getElementById("gameover").textContent = "Game Over - there are no more moves available, you made " + movesUsed + " moves.";
+        document.getElementById("gameover").textContent = "Game Over - there are no more moves available, you made " + movesUsed + " moves out of a possible " + (height * width);
     }
 }
